523e17f688cabb1a5b89f5a9d42c7dd3da70e168
Kakika85 <144158769+Kakika85@users.noreply.github.com>
2024-04-20 11:51:37 +0200

    created project

---
+++ src/main/java/com/example/SpaceProject/SpaceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpaceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpaceProject/SpaceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpaceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

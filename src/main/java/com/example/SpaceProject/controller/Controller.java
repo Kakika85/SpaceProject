51ae932a92abb85fec8494800b29066f90592454
Martin Blazek <martinblazek6@gmail.com>
2024-04-20 18:41:21 +0200

    conflicts resolved

--- src/main/java/com/example/SpaceProject/controller/Controller.java
+++ src/main/java/com/example/SpaceProject/controller/Controller.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @org.springframework.stereotype.Controller
 @RestController
 @RequiredArgsConstructor

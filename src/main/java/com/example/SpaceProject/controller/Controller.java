f5511fdede79a30754c21f05fafa542a8500614b
Frolíková Karolina <frolikova.karolina@gmail.com>
2024-04-28 15:20:01 +0200

    příprava na postman request

--- src/main/java/com/example/SpaceProject/controller/Controller.java
+++ src/main/java/com/example/SpaceProject/controller/Controller.java
@@ -3,23 +3,17 @@ package com.example.SpaceProject.controller;
 import com.example.SpaceProject.entity.AssignAstronautToCraft;
 import com.example.SpaceProject.entity.Astronaut;
 import com.example.SpaceProject.entity.Craft;
-import com.example.SpaceProject.repository.AstronautRepository;
-import com.example.SpaceProject.repository.CraftRepository;
 import com.example.SpaceProject.service.AstronautService;
 import com.example.SpaceProject.service.CraftService;
 import com.example.SpaceProject.service.UtilServices;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,8 +23,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.hibernate.query.sqm.tree.SqmNode.log;
 
 @RestController
 @RequiredArgsConstructor
